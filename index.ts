ab1e9b6661900df1cb095585a93d3bf4cfd2fbc0
Lucas Santos <lhs.santoss@gmail.com>
2020-05-28 15:47:04 -0300

    ✨ Finish example

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 const url = Deno.args[0]
 const res = await fetch(url)
 
+// JSON Version
+console.log('JSON Version')
 const body = await res.json()
 console.log(body)
+
+// Stream Version
+console.log('Stream Version')
+const bodyStream = new Uint8Array(await res.arrayBuffer())
+await Deno.stdout.write(bodyStream)
